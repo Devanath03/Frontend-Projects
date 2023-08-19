92f576be341fcd7b03d83e72d86802bee77c1c76
Devanath03 <devanathbalann@gmail.com>
2023-08-19 12:38:57 +0530

    Commit

--- QR Code Generator/script.js
+++ QR Code Generator/script.js
@@ -1,56 +1,40 @@
-let btn = document.querySelector(".button");
-let qr_code_element = document.querySelector(".qr-code");
+const container = document.querySelector(".container"),
+      qrInput = document.querySelector(".form input"),
+      generateQR = document.querySelector(".form button"),
+      qrImg = document.querySelector(".qrCode img"),
+      download  = document.querySelector(".download button");
 
-btn.addEventListener("click", () => {
-  let user_input = document.querySelector("#input_text");
-  if (user_input.value != "") {
-    if (qr_code_element.childElementCount == 0) {
-      generate(user_input);
-    } else {
-      qr_code_element.innerHTML = "";
-      generate(user_input);
-    }
-  } else {
-    console.log("not valid input");
-    qr_code_element.style = "display: none";
-  }
+generateQR.addEventListener("click", () => {
+    qrValue = qrInput.value;
+    console.log(qrValue);
+
+    if(!qrValue) return;
+
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+
+    qrImg.addEventListener("load", () => {
+        container.classList.add("active");
+        generateQR.innerText = "QR Generated";
+    })
 });
 
-function generate(user_input) {
-  qr_code_element.style = "";
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value){
+        container.classList.remove("active");
+        generateQR.innerText = "Generate QR";
+    }
+});
 
-  var qrcode = new QRCode(qr_code_element, {
-    text: `${user_input.value}`,
-    width: 180, //128
-    height: 180,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.H
-  });
-
-  let download = document.createElement("button");
-  qr_code_element.appendChild(download);
-
-  let download_link = document.createElement("a");
-  download_link.setAttribute("download", "qr_code.png");
-  download_link.innerHTML = `Download <i class="fa-solid fa-download"></i>`;
-
-  download.appendChild(download_link);
-
-  let qr_code_img = document.querySelector(".qr-code img");
-  let qr_code_canvas = document.querySelector("canvas");
-
-  if (qr_code_img.getAttribute("src") == null) {
-    setTimeout(() => {
-      download_link.setAttribute("href", `${qr_code_canvas.toDataURL()}`);
-    }, 300);
-  } else {
-    setTimeout(() => {
-      download_link.setAttribute("href", `${qr_code_img.getAttribute("src")}`);
-    }, 300);
-  }
-}
-
-generate({
-  value: "https://www.linkedin.com/in/devanath-b-732b0a21a/"
+download.addEventListener("click", () => {
+    console.log("Invoked");
+    const imgURL = qrImg.src;
+    fetch(imgURL)
+        .then(response => response.blob())
+        .then(blob => {
+            const download_link = document.createElement("a");
+            download_link.href = URL.createObjectURL(blob);
+            download_link.download = "qrcode.png";
+            download_link.click();
+            URL.revokeObjectURL(download_link.href);
+        });
 });
