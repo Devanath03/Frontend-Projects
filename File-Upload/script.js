9a6a799b684057ae7dd0772ce18fcdf62df2e903
Devanath03 <devanathbalann@gmail.com>
2023-09-02 20:12:23 +0530

    Commit

--- File-Upload/script.js
+++ File-Upload/script.js
@@ -12,6 +12,7 @@ window.addEventListener("load", () => {
     })
 
     const fileshow = (fileName, fileType) => {
+        
         //For creating a new div for file-uploading
         const showfileboxElem = document.createElement("div");
         showfileboxElem.classList.add("showfilebox");
