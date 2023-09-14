19f54bae77e980b94d5be0396c58bb70dc470722
Devanath03 <devanathbalann@gmail.com>
2023-09-14 21:39:56 +0530

    Commit

--- File-Upload/script.js
+++ File-Upload/script.js
@@ -22,7 +22,7 @@ window.addEventListener("load", () => {
         leftElem.classList.add("left"); 
 
         //Creating span element for adding it inside div 
-        const fileTypeElem = document.createElement("span");
+        const fileTypeElem = document.createElement("span"); 
         fileTypeElem.classList.add("filetype");
         fileTypeElem.innerHTML = fileType;
         leftElem.append(fileTypeElem);
