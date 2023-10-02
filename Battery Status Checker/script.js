272f1f3d88a6ba624613f971fd6596b75a70ac9c
Devanath03 <devanathbalann@gmail.com>
2023-10-02 16:32:49 +0530

    Commit

--- Battery Status Checker/script.js
+++ Battery Status Checker/script.js
@@ -70,7 +70,7 @@ navigator.getBattery().then((battery) => {
         (chargingBar.style.animationIterationCount = "initial"));
   }
 
-  //   updating the Discharging Information
+  //   updating the Discharging Information changes
 
   function updateDischargingInfo() {
     const dischargeTime = parseInt(battery.dischargingTime / 60) ? true : false;
@@ -83,3 +83,4 @@ navigator.getBattery().then((battery) => {
       : (otherInfo.style.display = "none");
   }
 });
+
