d57b921d1efc00c6e79d5b117f71c77de27441ae
Devanath03 <devanathbalann@gmail.com>
2023-11-08 20:47:49 +0530

    Commit

--- Bar Code Generator/script.js
+++ Bar Code Generator/script.js
@@ -2,6 +2,7 @@ let userInput = document.querySelector('.container .userInput');
 let generateBtn = document.querySelector('.container .generate-btn');
 let barcode = document.querySelector('.container .barcode-box #barcode');
 let container = document.querySelector('.container');
+let downloadBtn = document.querySelector('.download-btn');
 
 let generateBarcode = () => {
         container.classList.add('active');
@@ -17,4 +18,18 @@ generateBtn.addEventListener('click', () => {
         if (userInput.value != '') {
                 generateBarcode();
         }
+});
+
+downloadBtn.addEventListener('click', () => {
+        console.log(userInput.value);
+        const imgURL = barcode.src;
+        fetch(imgURL)
+            .then(response => response.blob())
+            .then(blob => {
+                const download_link = document.createElement("a");
+                download_link.href = URL.createObjectURL(blob);
+                download_link.download = "barcode.png";
+                download_link.click();
+                URL.revokeObjectURL(download_link.href);
+            });
 });
