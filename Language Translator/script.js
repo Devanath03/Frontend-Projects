7056702453a389138902157c7e7aa3e3332207ca
Devanath03 <devanathbalann@gmail.com>
2024-06-08 22:26:54 +0530

    Commit

--- Language Translator/script.js
+++ Language Translator/script.js
@@ -96,7 +96,7 @@ const languages = {
     "xh-ZA": "Xhosa",
     "yi-YD": "Yiddish",
     "zu-ZA": "Zulu",
-  };12345
+  };123456
   
   const fromText = document.querySelector(".from-text"),
     toText = document.querySelector(".to-text"),
