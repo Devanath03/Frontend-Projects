c2c4160f0a947749edb3d0390dfa518222c3bc0d
Devanath03 <devanathbalann@gmail.com>
2024-09-05 16:06:31 +0530

    Commit

--- Code Editor/script.js
+++ Code Editor/script.js
@@ -1,7 +1,7 @@
 const htmlcode = document.querySelector("#html");
 const csscode = document.querySelector("#css");
 const jscode = document.querySelector("#js");
-const output = document.querySelector("#output");123
+const output = document.querySelector("#output");1234
 
 function run() {
 
