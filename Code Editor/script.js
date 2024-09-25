d5676ef77a3a0bde9c1e1687ee04ac86a8537180
Devanath03 <devanathbalann@gmail.com>
2024-09-25 19:23:31 +0530

    Commit

--- Code Editor/script.js
+++ Code Editor/script.js
@@ -19,7 +19,7 @@ function run() {
 htmlcode.addEventListener("keyup",()=>{run()})
 csscode.addEventListener("keyup",()=>{run()})
 jscode.addEventListener("keyup",()=>{run()})
-1234
+12345
 // Accessing data stored in Local Storage.
 htmlcode.value = localStorage.htmlcode;
 csscode.value = localStorage.csscode;
