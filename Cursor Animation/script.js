6c0fc974e1dc0989f30be566c91613522f8bd317
Devanath03 <devanathbalann@gmail.com>
2024-10-14 15:41:50 +0530

    Commit

--- Cursor Animation/script.js
+++ Cursor Animation/script.js
@@ -30,7 +30,7 @@ document.body.addEventListener('click', () => {
     pc.uniforms.uNoiseIntensity.value = 0.0001 + Math.random() * 0.001;
     pc.uniforms.uPointSize.value = 1 + Math.random() * 10;
 });
-123456789
+1234567890
 document.body.addEventListener('click', () => { 
     pc.uniforms.uColor.value.set(Math.random() * 0xffffff);
     pc.uniforms.uCoordScale.value = 0.001 + Math.random() * 2;
