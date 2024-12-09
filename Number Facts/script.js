7e566683ba98156393b179a391c55f79a90dcab3
Devanath03 <devanathbalann@gmail.com>
2024-12-09 21:16:25 +0530

    Commit

--- Number Facts/script.js
+++ Number Facts/script.js
@@ -9,7 +9,7 @@ const url = "http://numbersapi.com/";
   fact.innerHTML = `Input field cannot be empty`;
   return;
  }
-12345678
+123456789
  fetch(url + number,{
     headers:{
       'x-requested-with': 'text/plain'
