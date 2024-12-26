3465fed82aebdf37fe7f5656caf38464fee548f7
Devanath03 <devanathbalann@gmail.com>
2024-12-26 20:51:44 +0530

    Commit

--- Movie App/script.js
+++ Movie App/script.js
@@ -4,7 +4,7 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     `https://api.themoviedb.org/3/search/movie?&api_key=${APIKEY}&query=`;
 
-
+1
 const main = document.getElementById("content");
 const form = document.querySelector(".form");
 const search = document.getElementById("search");
