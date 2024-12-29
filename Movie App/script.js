4418ebd81504465fd2db5f2caca606cb5300b5a9
Devanath03 <devanathbalann@gmail.com>
2024-12-29 22:55:42 +0530

    Commit

--- Movie App/script.js
+++ Movie App/script.js
@@ -11,7 +11,7 @@ const search = document.getElementById("search");
 
 // initially get fav movies
 getMovies(APIURL);
-1
+12
 async function getMovies(url) {
     const resp = await fetch(url);
     const respData = await resp.json();
