4f0f6f0c77ca50323c7362a0a0d3d9b7834daecd
Devanath03 <devanathbalann@gmail.com>
2025-01-25 22:48:36 +0530

    Commit

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -51,5 +51,5 @@ setInterval(() => {
     date.innerHTML = dd;
     month.innerHTML = mon;
     year.innerHTML = yr;
-    // let mn = new Date().getMonth();
+    // let mn = new Date().getMonth();1
 }, 1000); //function get executed after 1 seconds
